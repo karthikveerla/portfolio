86f7e7a124570e4a42cb1f6a1151dfadbfa6a54c
Karthik Veerla <karthikveerla28@gmail.com>
2025-12-04 18:15:42 -0500

    testing

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   <div className="intro flex-container">
     <div className="intro-text">
       <h1 className="home__name">Karthik Veerla</h1>
-      <span className="home__education">I'm a Full-Stack Software Developer</span>
+      <span className="home__education">I'm a Full-Stack Java Software Developer</span>
       <HeaderSocials />
       <a href="#contact" className="btn">Contact Me</a>
     </div>
